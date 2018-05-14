914fdb09fbf150425430c915be4a604687f92b50
柱 沈 <352840792@qq.com>
2018-05-14 09:14:21 +0800

    提交

--- Design/Design.SimpleFactory/Program.cs
+++ Design/Design.SimpleFactory/Program.cs
@@ -7,6 +7,7 @@ namespace Design.SimpleFactory
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
