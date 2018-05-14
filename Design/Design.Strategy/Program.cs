e58d5669b943fe52574f387dafa4ac837243060a
柱 沈 <352840792@qq.com>
2018-05-14 09:53:25 +0800

    文字修改

---
+++ Design/Design.Strategy/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Design.Strategy
+{
+    /// <summary>
+    /// 策略模式
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
